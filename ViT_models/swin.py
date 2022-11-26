b834b7a81d3416966befd6b2b94c91877ec0f8ae
rusthigh <rusthigh@proton.me>
2022-11-26 07:07:43 +0000

    md update

--- ViT_models/swin.py
+++ ViT_models/swin.py
@@ -478,4 +478,202 @@ class SwinTransformer(nn.Module):
         attn_drop_rate (float): Attention dropout rate. Default: 0
         drop_path_rate (float): Stochastic depth rate. Default: 0.1
         norm_layer (nn.Module): Normalization layer. Default: nn.LayerNorm.
-        ape (bool): If True, add absolute position 
+        ape (bool): If True, add absolute position embedding to the patch embedding. Default: False
+        patch_norm (bool): If True, add normalization after patch embedding. Default: True
+        use_checkpoint (bool): Whether to use checkpointing to save memory. Default: False
+    """
+
+    def __init__(self, img_size=224, patch_size=4, in_chans=3, num_classes=1000,
+                 embed_dim=96, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
+                 window_size=7, mlp_ratio=4., qkv_bias=True, qk_scale=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
+                 norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
+                 use_checkpoint=False, **kwargs):
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.num_layers = len(depths)
+        self.embed_dim = embed_dim
+        self.ape = ape
+        self.patch_norm = patch_norm
+        self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
+        self.mlp_ratio = mlp_ratio
+
+        # split image into non-overlapping patches
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+        num_patches = self.patch_embed.num_patches
+        patches_resolution = self.patch_embed.patches_resolution
+        self.patches_resolution = patches_resolution
+
+        # absolute position embedding
+        if self.ape:
+            self.absolute_pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+            trunc_normal_(self.absolute_pos_embed, std=.02)
+
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        # stochastic depth
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+
+        # build layers
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layer = BasicLayer(dim=int(embed_dim * 2 ** i_layer),
+                               input_resolution=(patches_resolution[0] // (2 ** i_layer),
+                                                 patches_resolution[1] // (2 ** i_layer)),
+                               depth=depths[i_layer],
+                               num_heads=num_heads[i_layer],
+                               window_size=window_size,
+                               mlp_ratio=self.mlp_ratio,
+                               qkv_bias=qkv_bias, qk_scale=qk_scale,
+                               drop=drop_rate, attn_drop=attn_drop_rate,
+                               drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
+                               norm_layer=norm_layer,
+                               downsample=PatchMerging if (i_layer < self.num_layers - 1) else None,
+                               use_checkpoint=use_checkpoint)
+            self.layers.append(layer)
+
+        self.norm = norm_layer(self.num_features)
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'absolute_pos_embed'}
+
+    @torch.jit.ignore
+    def no_weight_decay_keywords(self):
+        return {'relative_position_bias_table'}
+
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        if self.ape:
+            x = x + self.absolute_pos_embed
+        x = self.pos_drop(x)
+
+        for layer in self.layers:
+            x = layer(x)
+
+        #print(x.size())
+        x = self.norm(x)  # B L C
+        x = self.avgpool(x.transpose(1, 2))  # B C 1
+        #print(x.size())
+        x = torch.flatten(x, 1)
+        #print(x.size())
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+    def flops(self):
+        flops = 0
+        flops += self.patch_embed.flops()
+        for i, layer in enumerate(self.layers):
+            flops += layer.flops()
+        flops += self.num_features * self.patches_resolution[0] * self.patches_resolution[1] // (2 ** self.num_layers)
+        flops += self.num_features * self.num_classes
+        return flops
+
+def swin_t(pretrained=False, task_cls=10, **kwargs):
+    model = SwinTransformer(img_size=224, patch_size=4, in_chans=3, num_classes=1000, embed_dim=96,
+                            depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24], window_size=7, mlp_ratio=4,
+                            qkv_bias=True, qk_scale=None, drop_rate=0., drop_path_rate=0.2,
+                            ape=False, patch_norm=True, use_checkpoint=False)
+    
+    if pretrained:
+        checkpoint = torch.load(
+            model_urls['swin_t'],
+            map_location="cpu"
+            )
+        model.load_state_dict(checkpoint['model'])
+        print("Pretrained Models Loaded")
+    embed_dim = model.head.in_features
+    num_classes = task_cls
+    model.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+    _init_weights(model.head)
+
+    return model
+
+def swin_s(pretrained=False, task_cls=10, **kwargs):
+    model = SwinTransformer(img_size=224, patch_size=4, in_chans=3, num_classes=1000, embed_dim=96,
+                            depths=[2, 2, 18, 2], num_heads=[3, 6, 12, 24], window_size=7, mlp_ratio=4,
+                            qkv_bias=True, qk_scale=None, drop_rate=0., drop_path_rate=0.3,
+                            ape=False, patch_norm=True, use_checkpoint=False)
+    
+    if pretrained:
+        checkpoint = torch.load(
+            model_urls['swin_s'],
+            map_location="cpu"
+            )
+        model.load_state_dict(checkpoint['model'])
+        print("Pretrained Models Loaded")
+    embed_dim = model.head.in_features
+    num_classes = task_cls
+    model.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+    _init_weights(model.head)
+    
+    return model
+
+def swin_b(pretrained=False, task_cls=10, **kwargs):
+    model = SwinTransformer(img_size=224, patch_size=4, in_chans=3, num_classes=1000, embed_dim=128,
+                            depths=[2, 2, 18, 2], num_heads=[4, 8, 16, 32], window_size=7, mlp_ratio=4,
+                            qkv_bias=True, qk_scale=None, drop_rate=0., drop_path_rate=0.5,
+                            ape=False, patch_norm=True, use_checkpoint=False)
+    if pretrained:
+        checkpoint = torch.load(
+            model_urls['swin_b'],
+            map_location="cpu"
+            )
+        model.load_state_dict(checkpoint['model'])
+        print("Pretrained Models Loaded")
+    embed_dim = model.head.in_features
+    num_classes = task_cls
+    model.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+    _init_weights(model.head)
+
+    return model
+
+if __name__ == '__main__':
+    model = swin_s(pretrained=False)
+    fc_layer = model.head
+    features = []
+    def hook_fn_forward(module, input, output):
+        #features.append(input[0].detach().cpu())
+        # print(input.shape,'input.shape')
+        # print(input[0].detach().shape,'input[0].detach().shape')  torch.Size([256, 1024])
+        features.append(input[0].detach().cpu())
+    forward_hook = fc_layer.register_forward_hook(hook_fn_forward)
+    
+    
+    # print(model)
+    for n, m in model.named_modules():
+        print(n,m)
+    x = torch.rand(2,3,224,224)
+    y=model(x)
+    forward_hook.remove()
+    print(y.size())
+    
+    print(len(features))
+    print(features[0].shape)
+    avgpool = nn.AdaptiveAvgPool1d(1).cuda()
+    features = torch.cat([torch.flatten(avgpool(x.transpose(1, 2)), 1) for x in features])
+    print(features.shape)
+
+    # orch.Size([2, 768, 49])  anorm
+    # torch.Size([2, 49, 768]) bnorm
+    
